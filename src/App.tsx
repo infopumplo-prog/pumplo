d5944c9182ca67791cca87ad4ef1e033d67abbbc
David Novotny <info.pumplo@gmail.com>
2026-04-27 18:35:03 +0200

    chore: remove test comment from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// test dev agent
 import { useState, useEffect, lazy, Suspense } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
